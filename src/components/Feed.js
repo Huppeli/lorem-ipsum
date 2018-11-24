e31de1095d4340a571d953fdb379afbae7def738
Huppeli <mika.manninen93@gmail.com>
2018-11-24 14:37:15 +0200

    added feed page

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Typography } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Feed = () => <h2>Cofffffffeeee</h2>;
+const Feed = () => 
+    <div>
+        <AppBar position="static" color="#fffff">
+            <Toolbar>
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">
+                    Feed
+                </Typography>            
+            </Toolbar>
+        </AppBar>
+        <Typography variant="h6">Some content</Typography>
+    </div>
+    ;
 
 export default Feed;
